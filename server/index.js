f3c035b239a86bb35ef7bcf47301c834eb996479
Andrew Gil <andrewgil@Andrews-MacBook-Air.local>
2022-11-02 19:30:20 -0400

    change localhost stuff from 5000 to 4000

--- server/index.js
+++ server/index.js
@@ -16,8 +16,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+//const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://user:AGbTx8wpRtqPDQTD@cluster0.p8ccqgw.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT|| 4000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
